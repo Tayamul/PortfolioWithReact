6efbf5ec08b6a189e0ed696241463f9c22998113
Tayamul <rai_tayamul@hotmail.com>
2023-03-19 22:30:52 +0000

    Import Home component in the App file and render it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Tayamul Rai</h1>
+      <Home/>
     </div>
   );
 }
