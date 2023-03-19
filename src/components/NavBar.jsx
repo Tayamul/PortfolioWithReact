ae585b4bb93b533c89121cb1d2e01a2f32465da2
Tayamul <rai_tayamul@hotmail.com>
2023-03-19 23:45:37 +0000

    Keep the position fixed of the nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <header className='container nav__container'>
         <p>Tayamul Rai</p>
-        <ul>
+        <ul className='nav-list'>
             <li><a href='#home'>Home</a></li>
             <li><a href='#about'>About</a></li>
             <li><a href='#projects'>Projects</a></li>
