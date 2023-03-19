90367f51119860f37c25b0b2a2ec4dea8c905e44
Tayamul <rai_tayamul@hotmail.com>
2023-03-19 22:45:48 +0000

    Add useful project links in the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,15 @@ import './navBar.css'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header>
+        <p></p>
+        <ul>
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#about'>About</a></li>
+            <li><a href='#projects'>Projects</a></li>
+            <li><a href='#contact'>Contact</a></li>
+        </ul>
+    </header>
   )
 }
 
