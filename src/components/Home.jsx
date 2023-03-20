abf94c3897e876ef8cf690ce377e722b7175155b
Tayamul <rai_tayamul@hotmail.com>
2023-03-20 00:28:16 +0000

    Insert several p tags to help style the navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi incidunt consequuntur distinctio laudantium sequi ipsum veritatis amet pariatur ducimus, quibusdam ab rerum atque error voluptatum, aut porro deleniti tempore esse?</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi incidunt consequuntur distinctio laudantium sequi ipsum veritatis amet pariatur ducimus, quibusdam ab rerum atque error voluptatum, aut porro deleniti tempore esse?</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi incidunt consequuntur distinctio laudantium sequi ipsum veritatis amet pariatur ducimus, quibusdam ab rerum atque error voluptatum, aut porro deleniti tempore esse?</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi incidunt consequuntur distinctio laudantium sequi ipsum veritatis amet pariatur ducimus, quibusdam ab rerum atque error voluptatum, aut porro deleniti tempore esse?</p>
+    </div>
   )
 }
 
