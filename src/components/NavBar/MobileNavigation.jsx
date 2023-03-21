bb8401288e566079111277ddc53395a07067c6e3
Tayamul <rai_tayamul@hotmail.com>
2023-03-21 18:58:54 +0000

    Render burger menu in the page

--- src/components/NavBar/MobileNavigation.jsx
+++ src/components/NavBar/MobileNavigation.jsx
@@ -1,9 +1,11 @@
 import NavLinks from "./NavLinks";
 import classes from './NavBar.module.css'
+import {CgMenu} from 'react-icons/cg'
 
 const MobileNavigation = () => {
   return (
     <nav className={classes.MobileNavigation}>
+      <CgMenu className={classes.Hamburger} size="25px" color='#151513' />
       <NavLinks />
     </nav>
   );
