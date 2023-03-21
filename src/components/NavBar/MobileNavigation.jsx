d46e9690661ad295ad0f47bfb326f7c23f3e2d4b
Tayamul <rai_tayamul@hotmail.com>
2023-03-21 19:33:36 +0000

    Use open state for enabling nav links to appear when burger icon clicked by the user

--- src/components/NavBar/MobileNavigation.jsx
+++ src/components/NavBar/MobileNavigation.jsx
@@ -1,12 +1,20 @@
 import NavLinks from "./NavLinks";
-import classes from './NavBar.module.css'
-import {CgMenu} from 'react-icons/cg'
+import classes from "./NavBar.module.css";
+import { CgMenu } from "react-icons/cg";
+import { useState } from "react";
 
 const MobileNavigation = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className={classes.MobileNavigation}>
-      <CgMenu className={classes.Hamburger} size="25px" color='#151513' />
-      <NavLinks />
+      <CgMenu
+        className={classes.Hamburger}
+        size="25px"
+        color="#151513"
+        onClick={() => setOpen(!open)}
+      />
+      {open && <NavLinks />}
     </nav>
   );
 };
