4bd8dce0f6d49c27c4945000b89b53890e135017
Tayamul <rai_tayamul@hotmail.com>
2023-03-21 17:29:32 +0000

    Replace all the required navigation links into the NavLinks component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,12 +5,6 @@ const NavBar = () => {
   return (
     <header className='container nav__container'>
         <p>Tayamul Rai</p>
-        <ul className='nav-list'>
-            <li><a href='#home'>Home</a></li>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#projects'>Projects</a></li>
-            <li><a href='#contact'>Contact</a></li>
-        </ul>
     </header>
   )
 }
--- src/components/NavBar/NavLinks.jsx
+++ src/components/NavBar/NavLinks.jsx
@@ -1,5 +1,12 @@
 const NavLinks = () => {
-    return ("");
+    return (
+        <ul className='nav-list'>
+        <li><a href='#home'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#projects'>Projects</a></li>
+        <li><a href='#contact'>Contact</a></li>
+    </ul>
+    );
 }
 
 export default NavLinks;
