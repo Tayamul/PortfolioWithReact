e63d8de34bcd2ae8b77dbf07c8ba92ccd74b187f
Tayamul <rai_tayamul@hotmail.com>
2023-03-21 21:35:23 +0000

    Add onClick event to the links list

--- src/components/NavBar/NavLinks.jsx
+++ src/components/NavBar/NavLinks.jsx
@@ -1,10 +1,10 @@
-const NavLinks = () => {
+const NavLinks = ({isMobile, closeMobileMenu}) => {
     return (
         <ul className='nav-list'>
-        <li><a href='#home'>Home</a></li>
-        <li><a href='#about'>About</a></li>
-        <li><a href='#projects'>Projects</a></li>
-        <li><a href='#contact'>Contact</a></li>
+        <li onClick={() => isMobile && closeMobileMenu()}><a href='#home'>Home</a></li>
+        <li onClick={() => isMobile && closeMobileMenu()}><a href='#about'>About</a></li>
+        <li onClick={() => isMobile && closeMobileMenu()}><a href='#projects'>Projects</a></li>
+        <li onClick={() => isMobile && closeMobileMenu()}><a href='#contact'>Contact</a></li>
     </ul>
     );
 }
