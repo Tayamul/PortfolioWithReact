074ec701a7db4600c7b664f9d2567ab01c62f760
Tayamul <rai_tayamul@hotmail.com>
2023-03-21 17:36:55 +0000

    Import and render both navigation component in to the main navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './navBar.css'
+import MobileNavigation from './MobileNavigation'
+import Navigation from './Navigation'
 
 const NavBar = () => {
   return (
     <header className='container nav__container'>
         <p>Tayamul Rai</p>
+        <Navigation/>
+        <MobileNavigation/>
     </header>
   )
 }
