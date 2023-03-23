7c25f50639dc86d3973ee68803ac2fec70770b7b
Tayamul <rai_tayamul@hotmail.com>
2023-03-23 11:10:28 +0000

    Render the projects component in the App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About/About';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
+import Projects from './components/Projects/Projects';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar/>
       <Home/>
       <About/>
+      <Projects />
     </div>
   );
 }
