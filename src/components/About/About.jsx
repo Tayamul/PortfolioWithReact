2e4703a35b848b29cc25aa5db3feaf4318ff0a90
Tayamul <rai_tayamul@hotmail.com>
2023-03-23 09:08:13 +0000

    Create a basic structure for the about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,16 @@ import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+        <div id='about-summary'>
+            <p>ABOUT ME</p>
+            <h1>A dedicated Front-End Developer based in London, UK 📍</h1>
+            <p>As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, MaterialUI, and Firebase. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimised code and utilising cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p>
+        </div>
+        <div id='about-img'>
+
+        </div>
+    </section>
   )
 }
 
