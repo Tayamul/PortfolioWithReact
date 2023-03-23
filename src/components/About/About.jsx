055d6113d2edb37676a82226824617161707424f
Tayamul <rai_tayamul@hotmail.com>
2023-03-23 09:36:13 +0000

    Finish shaping and structuring the about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section id='about'>
         <div id='about-summary'>
-            <p>ABOUT ME</p>
+            <p id='about-title'>ABOUT ME</p>
             <h1>A dedicated Front-End Developer based in London, UK 📍</h1>
             <p>As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, MaterialUI, and Firebase. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimised code and utilising cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p>
         </div>
