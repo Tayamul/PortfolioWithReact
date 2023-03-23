b270c86df421905af950dd98da4d8a617ac3470c
Tayamul <rai_tayamul@hotmail.com>
2023-03-23 11:20:10 +0000

    Design a basic layout for the projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,7 +3,19 @@ import './projects.css'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section id='projects'>
+        <p>PROJECTS</p>
+        <h1>Each project has its own feature and functionality</h1>
+        <div class='projects-container'>
+            <div id='projects-img'></div>
+            <div id='projects-summary'>
+                <h2>NEWS WEB</h2>
+                <p>A social discussion website enhancing user interaction with features including article rating, commenting, and article submission. </p>
+                <span><h5>React</h5><h5>CSS</h5><h5>Material UI</h5></span>
+                <span>Code Live Demo</span>
+            </div>
+        </div>
+    </section>
   )
 }
 
