03a9d82949446ab0ff2d76535a799c80b5b64e6b
Tayamul <rai_tayamul@hotmail.com>
2023-03-23 17:23:13 +0000

    Replace the div container with an actual image and resized it to improve responsiveness

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './about.css'
+import mac from '../../assets/mac.png'
 
 const About = () => {
   return (
@@ -9,9 +10,9 @@ const About = () => {
             <h1>A dedicated Front-End Developer based in London, UK 📍</h1>
             <p>As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, MaterialUI, and Firebase. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimised code and utilising cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p>
         </div>
-        <div id='about-img'>
-
-        </div>
+        {/* <div id='about-img'> */}
+          <img src={mac} alt='macbook' id='about-img'/>
+        {/* </div> */}
     </section>
   )
 }
