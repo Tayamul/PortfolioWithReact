1454405e40d152bb83e3e8130f176f6092273f96
Tayamul <rai_tayamul@hotmail.com>
2023-03-24 08:55:28 +0000

    Resize the image on About page for better resolution

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
-import './about.css'
-import mac from '../../assets/mac.png'
+import React from "react";
+import "./about.css";
+import mac from "../../assets/mac.png";
 
 const About = () => {
   return (
-    <section id='about'>
-        <div id='about-summary'>
-            <p id='about-title'>ABOUT ME</p>
-            <h1>A dedicated Front-End Developer based in London, UK 📍</h1>
-            <p>As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, MaterialUI, and Firebase. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimised code and utilising cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p>
-        </div>
-        {/* <div id='about-img'> */}
-          <img src={mac} alt='macbook' id='about-img'/>
-        {/* </div> */}
+    <section id="about">
+      <div id="about-summary">
+        <p id="about-title">ABOUT ME</p>
+        <h1>A dedicated Front-End Developer based in London, UK 📍</h1>
+        <p>
+          I excel in designing and maintaining responsive websites that offer a
+          smooth user experience.
+        </p>
+        <br />
+        <p>
+          My expertise lies in crafting dynamic, engaging interfaces through
+          writing clean and optimised code and utilising cutting-edge
+          development tools and techniques.{" "}
+        </p>
+        <br />
+        <p>
+          I am also a team player who thrives in collaborating with
+          cross-functional teams to produce outstanding web applications.
+        </p>
+      </div>
+      {/* <div id='about-img'> */}
+      <img src={mac} alt="macbook" id="about-img" />
+      {/* </div> */}
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
