cd4600a0d36da0938d01ded90368722042c6fd0c
Tayamul <rai_tayamul@hotmail.com>
2023-03-25 12:43:04 +0000

    Add and design second projects in the portfolio

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,6 +5,8 @@ import { BsArrowUpRightCircle } from "react-icons/bs";
 import { motion } from "framer-motion";
 import newsWeb from '../../assets/News-Web.png'
 import newsWeb2 from '../../assets/News-Web2.png'
+import todo from '../../assets/Todo-pic.png'
+import todo2 from '../../assets/Todo-pic2.png'
 
 const Projects = () => {
   return (
@@ -13,6 +15,7 @@ const Projects = () => {
         <p>PORTFOLIO</p>
         <h1>Each project has its own feature and functionality</h1>
 
+        {/* News Web */}
         <div className="projects-section">
           {/* <div className="projects-img"></div> */}
           <img src={newsWeb} alt='News web app screenshot' className="projects-img" />
@@ -31,7 +34,7 @@ const Projects = () => {
             </div>
             <div className="live-code">
               <motion.a
-                href="https://github.com/Tayamul"
+                href="https://github.com/Tayamul/News-Web"
                 target="_blank"
                 rel="noreferrer"
                 whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
@@ -52,6 +55,48 @@ const Projects = () => {
               </motion.a>
             </div>
           </div>
+        </div>
+        {/* Task Manager */}
+        <div className="projects-section-alt">
+          {/* <div className="projects-img"></div> */}
+          
+          <div className="projects-summary-alt">
+            <h2>Task Management Web-app</h2>
+            <p>
+              A social discussion website enhancing user interaction with
+              features including article rating, commenting, and article
+              submission.{" "}
+            </p>
+            <div className="tech-skills-alt">
+              <p>React</p>
+              <p>MaterialUI</p>
+              <p>Firebase</p>
+            </div>
+            <div className="live-code-alt">
+              <motion.a
+                href="https://github.com/Tayamul/to-do-web-app"
+                target="_blank"
+                rel="noreferrer"
+                whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
+                whileTap={{ scale: 0.9 }}
+              >
+                Code
+                <SiGithub style={{ marginLeft: "5px" }} />
+              </motion.a>
+              <motion.a
+                whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
+                whileTap={{ scale: 0.9 }}
+                href="https://to-do-app-382c5.web.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Live Demo
+                <BsArrowUpRightCircle style={{ marginLeft: "5px" }} />
+              </motion.a>
+            </div>
+          </div>
+          <img src={todo} alt='Task management web app screenshot' className="projects-img-alt" />
+          <img src={todo2} alt='Task management web app screenshot' className="projects-img2-alt" />
         </div>
       </div>
     </section>
