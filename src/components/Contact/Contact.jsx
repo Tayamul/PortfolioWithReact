803037d2f143f4ba4ab1764ee502d9e9cb36d3ac
Tayamul <rai_tayamul@hotmail.com>
2023-03-25 15:53:54 +0000

    Design the contact page and improving user interface

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import './contact.css'
 import { SiLinkedin } from "react-icons/si";
+import {HiOutlineMail} from 'react-icons/hi';
 
 const Contact = () => {
   return (
     <section id='contact'>
         <p>CONTACT</p>
         <h1>Drop me an email! Connect with me on LinkedIn!</h1>
-        <div id='contact-container'>
+        <div class='contact-container'>
             <div className='contact-content'>
-              <SiLinkedin/>
+              <SiLinkedin size={30}/>
               <div className='contact-text'>
                 <a href='https://www.linkedin.com/in/tayamul-rai' target='_blank' rel="noreferrer">
-                <p>LinkedIn</p>
+                <h6>LinkedIn</h6>
                 <p>linkedin.com/in/tayamul-rai</p>
+                </a>
+              </div>
+            </div>
+            <div className='contact-content'>
+              <HiOutlineMail size={35}/>
+              <div className='contact-text'>
+              <a href="mailto: rai_tayamul@hotmail.com" target="_blank" rel="noreferrer">
+                <h6>Mail</h6>
+                <p>rai_tayamul@hotmail.com</p>
                 </a>
               </div>
             </div>
@@ -21,6 +31,5 @@ const Contact = () => {
     </section>
   )
 }
-{/* <a href="mailto: rai_tayamul@hotmail.com" target="_blank" rel="noreferrer">Send an email</a> */}
 
 export default Contact
