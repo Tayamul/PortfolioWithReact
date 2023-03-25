6d32e154e237aebede0f9d0768e5fdd3f82dcc52
Tayamul <rai_tayamul@hotmail.com>
2023-03-25 14:13:30 +0000

    Design a contact page's basic layout

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './contact.css'
+import { SiLinkedin } from "react-icons/si";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id='contact'>
+        <p>CONTACT</p>
+        <h1>Drop me an email! Connect with me on LinkedIn!</h1>
+        <div id='contact-container'>
+            <div className='contact-content'>
+              <SiLinkedin/>
+              <div className='contact-text'>
+                <a href='https://www.linkedin.com/in/tayamul-rai' target='_blank' rel="noreferrer">
+                <p>LinkedIn</p>
+                <p>linkedin.com/in/tayamul-rai</p>
+                </a>
+              </div>
+            </div>
+        </div>
+    </section>
   )
 }
+{/* <a href="mailto: rai_tayamul@hotmail.com" target="_blank" rel="noreferrer">Send an email</a> */}
 
 export default Contact
