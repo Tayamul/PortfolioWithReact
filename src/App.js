04e7de9322d26e705522b998c9bc8db2431b08c1
Tayamul <rai_tayamul@hotmail.com>
2023-03-26 14:21:23 +0100

    Import the footer component in the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import Projects from './components/Projects/Projects';
@@ -13,6 +14,7 @@ function App() {
       <About/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
