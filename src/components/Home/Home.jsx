a8af0b64601f63b4e439d4b6c416d98c9d4a9ceb
Tayamul <rai_tayamul@hotmail.com>
2023-04-26 23:49:58 +0100

    Edit the subheading to make it more personalised

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ import css from "../../assets/css-3.svg";
 import js from "../../assets/javascript-1.svg";
 import node from "../../assets/nodejs-1.svg";
 import mui from "../../assets/material-ui-1.svg";
-import me from '../../assets/memoji.JPG'
+import me from "../../assets/memoji.JPG";
 import "./home.css";
 import { SiLinkedin } from "react-icons/si";
 import { SiGithub } from "react-icons/si";
@@ -22,43 +22,62 @@ const Home = () => {
         {/* <div className="content"> */}
         <div className="profile">
           {/* <div id="profile-pic"></div> */}
-          <img id="profile-pic" src={me} alt='Memoji of Tayamul'/>
+          <img id="profile-pic" src={me} alt="Memoji of Tayamul" />
           <div id="profile-summary">
-            <h1>Hi 👋🏻 I'm Tayamul Rai </h1> 
-            <p>
-              As a Software Developer, my expertise lies in creating seamless and intuitive user experiences. I specialise in using cutting-edge technologies to produce high-quality web applications.
-            </p>
+            <h1>Hi 👋🏻 I'm Tayamul Rai </h1>
             {/* <p>
-               I've just completed an intensive coding bootcamp and I'm eager to jump-start my career in the tech industry as a Junior Developer! <br/>I'm ready to apply my newly acquired skills and collaborate with a team to develop cutting-edge software that will revolutionize the world.
+              As a Software Developer, my expertise lies in creating seamless and intuitive user experiences. I specialise in using cutting-edge technologies to produce high-quality web applications.
             </p> */}
+            <p>
+              A recent coding bootcamp graduate eager to jump-start my career in
+              the tech industry.{" "}
+            </p>
+            {/* <br />{" "} */}
+            <p>
+              My love for technology drives me to stay current with emerging
+              trends.{" "}
+            </p>
+            {/* <br /> */}
+            <p>
+              Currently, I am learning Next.js with the intention of expanding
+              my technology stack.
+            </p>
+            {/* I'm ready to apply my newly acquired skills and collaborate with a team to develop cutting-edge software that will revolutionize the world. 
+            My love for programming grew when I stumbled upon a Wed Dev course at UDEMY.
+             I instantly fell in love with programming when my friend introduced me to it about a year ago.*/}
             <div id="socials">
               <motion.a
-                href='https://linkedin.com/in/tayamul-rai'
-                target='_blank'
-                rel='noreferrer'
+                href="https://linkedin.com/in/tayamul-rai"
+                target="_blank"
+                rel="noreferrer"
                 aria-label="Visit Tayamul's LinkedIn profile"
                 whileHover={{ scale: 1.2, transition: { duration: 0.3 } }}
                 whileTap={{ scale: 0.9 }}
               >
-                <SiLinkedin size="30" color='#2d2e32' />
+                <SiLinkedin size="30" color="#2d2e32" />
               </motion.a>
               <motion.a
-              href='https://github.com/Tayamul'
-              target='_blank'
-              rel='noreferrer'
-              aria-label="Visit Tayamul's Github profile"
-              whileHover={{ scale: 1.2, transition: { duration: 0.3 } }}
-              whileTap={{ scale: 0.9 }}>
-                <SiGithub size="30" color='#2d2e32' />
+                href="https://github.com/Tayamul"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Visit Tayamul's Github profile"
+                whileHover={{ scale: 1.2, transition: { duration: 0.3 } }}
+                whileTap={{ scale: 0.9 }}
+              >
+                <SiGithub size="30" color="#2d2e32" />
               </motion.a>
             </div>
             <div className="cta">
-              <a href='https://drive.google.com/file/d/1MyGkLw1HnLdYmTqNUEt_NDwK-OLTHMDM/view?usp=sharing' target='_blank' rel="noreferrer"><button className="cta-btn">
-                View CV
-              </button></a>
-              <a href='#contact'><button className="cta-btn2">
-                Stay in touch
-              </button></a>
+              <a
+                href="https://drive.google.com/file/d/1MyGkLw1HnLdYmTqNUEt_NDwK-OLTHMDM/view?usp=sharing"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button className="cta-btn">View CV</button>
+              </a>
+              <a href="#contact">
+                <button className="cta-btn2">Stay in touch</button>
+              </a>
             </div>
           </div>
         </div>
